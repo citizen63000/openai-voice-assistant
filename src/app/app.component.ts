e7447e8fa7470a6d5993d0dbe0a7a296efc8694c
citizen63000 <citizen.lantien@gmail.com>
2023-03-07 22:34:54 +0100

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from "../environments/environment";
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {VoiceRecognitionService} from "./service/voice-recognition.service";
 import {TextToSpeechService} from "./service/text-to-speech.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 
 @Injectable({
@@ -22,56 +22,67 @@ export class AppComponent implements OnInit {
     public voiceButtonColor = 'primary'
     private messages = [{'role': 'system', 'content': 'Vous êtes une IA très serviable'}];
 
-  constructor(private http: HttpClient, public voiceRecognition: VoiceRecognitionService, public textToSpeech: TextToSpeechService) { }
+    constructor(private http: HttpClient, public voiceRecognition: VoiceRecognitionService, public textToSpeech: TextToSpeechService) {
+    }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
 
-      // microphone authorization
-      navigator.mediaDevices.getUserMedia({audio: true});
-      // init recognition system
-      this.voiceRecognition.init();
-      // init textToSpeech system
-      this.textToSpeech.init();
-  }
+        // microphone authorization
+        navigator.mediaDevices.getUserMedia({audio: true});
+        // init recognition system
+        this.voiceRecognition.init();
+        // init textToSpeech system
+        this.textToSpeech.init();
+    }
 
-  voiceRecognitionSwitch() {
-      if (this.voiceRecognitionState == 0) {
-          this.voiceRecognition.start();
-          this.voiceRecognitionState = 1;
-          this.voiceButtonColor = 'warn'
-      } else {
-          this.voiceRecognition.stop();
-          this.voiceRecognitionState = 0;
-          this.voiceButtonColor = 'primary'
-          this.callChatGPT(this.voiceRecognition.text);
-          this.voiceRecognition.text = '';
-      }
-  }
+    voiceRecognitionSwitch() {
+        if (this.voiceRecognitionState == 0) {
+            this.voiceRecognition.start();
+            this.voiceRecognitionState = 1;
+            this.voiceButtonColor = 'warn'
+        } else {
+            this.voiceRecognition.stop();
+            this.voiceRecognitionState = 0;
+            this.voiceButtonColor = 'primary'
+            this.callChatGPT(this.voiceRecognition.text);
+            this.voiceRecognition.text = '';
+            this.voiceRecognition.tempWords = '';
+        }
+    }
 
-  callChatGPT(text) {
+    sendText(text) {
+        // this.text.value='';
 
-    this.conversation = this.conversation.concat('<br /><b>Me</b> : ' + text);
+    }
 
-    let headers = new HttpHeaders({
-      'Authorization': 'Bearer ' + environment.openAIApiKey,
-      'Content-Type': 'application/json'
-    });
+    /**
+     * https://platform.openai.com/docs/guides/chat/introduction
+     * @param text
+     */
+    callChatGPT(text) {
 
-    // need to push all the conversation each time to keep the context
-    this.messages.push({'role': 'user', 'content': text});
-    let requestData = { 'model': 'gpt-3.5-turbo', 'messages': this.messages}
+        this.conversation = this.conversation.concat('<br /><b>Me</b> : ' + text);
 
-    this.http.post<any>('https://api.openai.com/v1/chat/completions', JSON.stringify(requestData),{headers: headers})
-        .subscribe(data => {
-            let response = data.choices[0].message.content;
-            this.messages.push({'role': 'assistant', 'content': response});
-            let htmlResponse = response.replace(/\\n/g, '<br/>');
-            this.textToVoice(htmlResponse);
-            this.conversation = this.conversation.concat('<br /><b>ChatGPT</b> : ' + htmlResponse);
+        let headers = new HttpHeaders({
+            'Authorization': 'Bearer ' + environment.openAIApiKey,
+            'Content-Type': 'application/json'
         });
-  }
 
-  textToVoice(text) {
+        // need to push all the conversation each time to keep the context
+        this.messages.push({'role': 'user', 'content': text});
+        let requestData = {'model': 'gpt-3.5-turbo', 'messages': this.messages}
+
+        this.http.post<any>('https://api.openai.com/v1/chat/completions', JSON.stringify(requestData), {headers: headers})
+            .subscribe(data => {
+                let response = data.choices[0].message.content;
+                this.messages.push({'role': 'assistant', 'content': response});
+                let htmlResponse = response.replace(/\\n/g, '<br/>');
+                this.textToVoice(htmlResponse);
+                this.conversation = this.conversation.concat('<br /><b>ChatGPT</b> : ' + htmlResponse);
+            });
+    }
+
+    textToVoice(text) {
         this.textToSpeech.say(text);
-  }
+    }
 }
